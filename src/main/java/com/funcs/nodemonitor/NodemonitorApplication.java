a41f95d7b47d1fd83663949515cd6ed713ca2979
bendf <bendfunction@outlook.com>
2020-11-23 20:47:48 +0800

    init

---
+++ src/main/java/com/funcs/nodemonitor/NodemonitorApplication.java
@@ -0,0 +1,14 @@
+package com.funcs.nodemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodemonitorApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(NodemonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/funcs/nodemonitor/NodemonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funcs.nodemonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodemonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
