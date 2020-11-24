0cbf326353aaf218ee78a78c22091294c7948e47
bendf <bendfunction@outlook.com>
2020-11-24 12:08:53 +0800

    Result control.

---
+++ src/main/java/com/funcs/nodemonitor/lang/Result.java
@@ -0,0 +1,44 @@
+package com.funcs.nodemonitor.lang;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+// 200 -> success, other is failed
+
+@Data
+public class Result implements Serializable {
+
+    private int code;
+    private String msg;
+    private Object data;
+
+    public static Result succ(Object data) {
+        return succ(200, "success", data);
+    }
+
+    public static Result succ(int code, String msg, Object data) {
+        Result r = new Result();
+        r.setCode(code);
+        r.setMsg(msg);
+        r.setData(data);
+        return r;
+    }
+
+    public static Result fail(String msg) {
+        return fail(400, msg, null);
+    }
+
+    public static Result fail(String msg, Object data) {
+        return fail(400, msg, data);
+    }
+
+    public static Result fail(int code, String msg, Object data) {
+        Result r = new Result();
+        r.setCode(code);
+        r.setMsg(msg);
+        r.setData(data);
+        return r;
+    }
+
+}
