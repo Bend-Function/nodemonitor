39c4a334de8594403487554625cbcb567c8f5697
bendf <bendfunction@outlook.com>
2020-11-27 11:53:25 +0800

    Add func -> search one server information.

--- src/main/java/com/funcs/nodemonitor/controller/ServerController.java
+++ src/main/java/com/funcs/nodemonitor/controller/ServerController.java
@@ -7,6 +7,7 @@ import com.funcs.nodemonitor.mapper.ServerMapper;
 import com.funcs.nodemonitor.service.ServerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -29,10 +30,16 @@ public class ServerController {
     ServerMapper serverMapper;
     @RequestMapping("/getall")
     public Result getAll(){
-//        List serverList = serverService.list();
-//        return Result.succ(serverList);
         QueryWrapper<Server> queryWrapper = new QueryWrapper<>();
         queryWrapper.select("id", "servername", "serverlocation", "serverdescribe");
+        List serverList = serverService.listMaps(queryWrapper);
+        return  Result.succ(serverList);
+    }
+
+    @GetMapping("/getone/{id}")
+    public Result getOne(@PathVariable(name = "id") Long id){
+        QueryWrapper<Server> queryWrapper = new QueryWrapper<>();
+        queryWrapper.select("id", "servername", "serverlocation", "serverdescribe").eq("id", id);
         List serverList = serverService.listMaps(queryWrapper);
         return  Result.succ(serverList);
     }
